91a44fbc28c33198c8ff726dfedb0d2b6a43ca21
Elliot Kwilinski <ekwilinski@mines.edu>
2021-10-07 18:54:43 -0600

    File input

--- algobowl.cpp
+++ algobowl.cpp
@@ -1,8 +1,40 @@
 // AlgoBowl
-
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
 using namespace std;
 int main() {
-    std::cout << "Hello World!";
+    ifstream f;
+
+    f.open("input.txt");
+
+    string numElements;
+    f >> numElements;
+    string numSets;
+    f >> numSets;
+
+    map<set<int>,int> subsets;
+
+    while(!f.eof()) {
+        stringstream s;
+        string line;
+        getline(f,line);
+        s << line;
+        set<int> subset;
+
+        while(!s.eof()) {
+            string num;
+            s >> num;
+            subset.emplace(stoi(num));
+        }
+
+        f >> line;
+        subsets.emplace(subset,stoi(line));
+    }   
+    f.close();
     return 0;
+
 }
