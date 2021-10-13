3b1b7a7d32999208d43b430d02c39e629c591379
Elliot Kwilinski <ekwilinski@mines.edu>
2021-10-13 15:46:53 -0600

    fixed 312 output

--- algobowl.cpp
+++ algobowl.cpp
@@ -18,7 +18,7 @@ bool sortbysec(const tuple<set<int>,double>& a, const tuple<set<int>, double>& b
 
 int main(int argc, char** argv) {
     ifstream f;
-    f.open("inputs/input_rand.txt");
+    f.open("inputs/input_group312.txt");
 
     string numElements;
     getline(f,numElements);
